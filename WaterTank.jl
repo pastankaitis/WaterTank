ccded5e232c81360024f6c02eec3efddb2136950
pastankaitis <p.stankaitis@ncl.ac.uk>
2021-08-31 16:24:33 +0100

    Add files via upload

---
+++ WaterTank.jl
@@ -0,0 +1,137 @@
+using ReachabilityAnalysis, Plots, Symbolics
+
+
+#constants
+
+const H_max = 3 # Maximum height of the tank 
+const B = 3 # Base of the cylinder tank
+const V_max = 3 # Maximum volume flow for the tank
+const V_saf_max = 2.9 # minimum allowed water level
+const V_saf_min = 0.5 # minimum allowed water level
+
+# water tank modes
+
+@taylorize function stable!(du, u, p, t)
+
+    #differential equations for the stable water tank mode
+    du[1] = 0
+    du[2] = 0
+    du[1] = 0
+
+    return du
+
+
+end
+
+@taylorize function filling!(du, u, p, t)
+
+
+    d_in, d_out, h = u
+    
+    #differential equations for the filling water tank mode
+    du[1] = 1
+    du[2] = 0
+    du[3] = d_in
+
+    return du
+
+end
+
+@taylorize function emptying!(du, u, p, t)
+
+    d_in, d_out, h = u
+
+    #differential equations for the emptying water tank mode
+    du[1] = 0
+    du[2] = 1
+    du[3] = -d_out
+
+    return du
+
+
+end
+
+@taylorize function normal!(du, u, p, t)
+
+    d_in, d_out, h = u
+
+    #differential equations for the normal water tank mode
+    du[1] = 1
+    du[2] = 1
+    du[3] = d_in - d_out
+
+    return du
+
+end
+
+function waterTank()
+
+    var = @variables d_in, d_out, h
+
+    automaton = LightAutomaton(4)
+    
+    #X = HPolyhedron([d_in > 0, d_out > 0, B * d_in <= V_max, B * d_out <= V_max], var) # invariants
+    #X = HPolyhedron([d_in > 0, d_out > 0], var) # invariants
+    stable_mode = @system(h' = stable!(h), dim:3) # water tank mode stable
+
+    #X = HPolyhedron([d_in > 0, d_out > 0, B * d_in <= V_max, B * d_out <= V_max], var) # invariants
+    #X = HPolyhedron([d_in > 0, d_out > 0], var) # invariants
+    normal_mode = @system(h' = normal!(h), dim:3) # water tank normal mode
+
+    #X = HPolyhedron([d_in > 0, d_out > 0, B * d_in <= V_max, B * d_out <= V_max], var) # invariants
+    #X = HPolyhedron([d_in > 0, d_out > 0], var) # invariants
+    filling_mode = @system(h' = filling!(h), dim:3) # water tank filling mode
+
+    #X = HPolyhedron([d_in > 0, d_out > 0, B * d_in <= V_max, B * d_out <= V_max], var) # invariants
+    #X = HPolyhedron([d_in > 0, d_out > 0], var) # invariants
+    emptying_mode = @system(h' = emptying!(h), dim:3) # water tank emptying mode
+
+
+    # transition "stable" -> "normal"
+    add_transition!(automaton, 1, 2, 1)
+    #guard_1 = HalfSpace(h >= 0, var)
+    t1 = @map(h -> h, dim: 3)
+
+   
+    # transition "normal" -> "filling"
+    add_transition!(automaton, 2, 3, 2)
+    guard_2 = HalfSpace(h <= V_saf_min, var)
+    t2 = @map(h -> h, dim: 3, h ∈ guard_2)
+
+    # transition "normal" -> "emptying"
+    add_transition!(automaton, 2, 3, 2)
+    guard_3 = HalfSpace(h >= V_saf_max, var)
+    t3 = @map(h -> h, dim: 3, h ∈ guard_3)
+
+
+    H = HybridSystem(automaton=automaton,
+                     modes=[stable_mode, normal_mode, filling_mode, emptying_mode],
+                     resetmaps=[t1, t2, t3])
+
+
+    ## initial condition in mode 1
+    X0 = Hyperrectangle([0.1, 0.1, 1.5], [0.5, 0.5, 2.0])
+    init = [(1, X0)]
+
+    return InitialValueProblem(H, init)
+
+
+end
+
+prob = waterTank()
+boxdirs = BoxDirections(3)
+
+sol12 = solve(prob, tspan=(0.0, 200.0),
+    alg=TMJets20(abstol=1e-5, maxsteps=10000, orderT=3, orderQ=1, disjointness=BoxEnclosure()),
+    intersect_source_invariant=false,
+    intersection_method=TemplateHullIntersection(boxdirs),
+    clustering_method=LazyClustering(1),
+    disjointness_method=BoxEnclosure())
+
+
+
+
+
+
+
+
